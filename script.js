8a605be3f65976c19db03147cf2a034917423c7f
Luiz Augusto Ferraz <luizaugustoferraz.dev@gmail.com>
2023-05-18 18:29:04 -0300

    Updating script

--- script.js
+++ script.js
@@ -4,41 +4,34 @@ calculate = () => {
     weight = document.getElementById("weight").value;
     height = document.getElementById("height").value;
     error = "Please enter a valid value";
-    height = height / 100;
-    height = height * height;
-    bmi = weight / height;
-    bmi = bmi.toFixed(1);
+    measure = "";
 
-    if(bmi <= 18.4){
-        measure = `Your BMI is ${bmi} which means you're underweight.`;
-    }
-    else if (bmi >= 18.5 && bmi <= 24.9){
-        measure = `Your BMI is ${bmi} which means your weight is normal.`;
-    }
-    else if (bmi >= 25 && bmi < 30){
-        measure = `Your BMI is ${bmi} which means you're overweight.`;
-    }
-    else if(bmi >= 30){
-        measure = `Your BMI is ${bmi} which means you're obese.`;
-    }
-
-    if (weight === 0){
-        document.getElementById("result").innerHTML=error;
-    }
-    else if(height === 0){
-        document.getElementById("result").innerHTML=error;
-    }
+    if (height > 0 && weight > 0){
+        height = height / 100;
+        height = height * height;
+        bmi = weight / height;
+        bmi = bmi.toFixed(1);
+        if (bmi <= 18.4){
+            measure = `Your BMI is ${bmi} which means you're underweight.`
+            document.getElementById("result").innerHTML=measure
+        }
+        else if (bmi >= 18.5 && bmi <= 24.9){
+            measure = `Your BMI is ${bmi} which means your weight is normal.`
+            document.getElementById("result").innerHTML=measure
+        }
+        else if (bmi >= 25 && bmi < 30){
+            measure = `Your BMI is ${bmi} which means you're overweight.`
+            document.getElementById("result").innerHTML=measure
+        }
+        else if (bmi >= 30){
+            measure = `Your BMI is ${bmi} which means you're obese.`
+            document.getElementById("result").innerHTML=measure
+        }
+        else{
+            document.getElementById("result").innerHTML=error
+        }
+        }
     else{
-        document.getElementById("result").innerHTML=measure;
-    }
-
-    if (weight < 0){
-        document.getElementById("result").innerHTML=error;
-    }
-    else if(height < 0){
-        document.getElementById("result").innerHTML=error;
-    }
-    else{
-        document.getElementById("result").innerHTML=measure; 
+        document.getElementById("result").innerHTML=error
     }
 }
